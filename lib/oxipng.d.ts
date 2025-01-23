7c46c76524cd6d1792a6a0d3ee51b5d39f0a0485
Rouven Weßling <me@rouvenwessling.de>
2025-01-23 14:15:12 +0100

    fix: d.ts file had duplicate definitions

--- lib/oxipng.d.ts
+++ lib/oxipng.d.ts
@@ -9,18 +9,3 @@ export type Options = {
 export function optimizeOxipngSync(input: Uint8Array, options?: Options) : Uint8Array;
 
 export function optimizeOxipng(input: Uint8Array, options?: Options) : Promise<Uint8Array>;
-
-export const enum InterlaceMode {
-  remove = 'remove',
-  apply = 'apply',
-  keep = 'keep'
-}
-export interface OxipngOptions {
-  force?: boolean
-  optimizationLevel?: number
-  optimizationMax?: boolean
-  optimizeAlpha?: boolean
-  interlace?: InterlaceMode
-}
-export declare function optimizeOxipngSync(input: Uint8Array, options: OxipngOptions): Uint8Array
-export declare function optimizeOxipng(input: Uint8Array, options: OxipngOptions): Promise<Uint8Array>
